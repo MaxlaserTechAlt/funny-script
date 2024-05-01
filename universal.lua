50ffbae4b762819de2f75ad8e8465b024539ed8b
MaxlaserTechAlt <pakorn.angpao@gmail.com>
2024-05-01 23:32:11 +0700

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -1,3 +1,13 @@
-local guilib = shared.kavo
+local guilib = shared.guiTab
 local entityFunction = shared.funnyentity
 local cloneref = cloneref or function(obj) return obj end
+local playerService = cloneref(game:GetService("Players"))
+local lplr = cloneref(game:GetService("Players").LocalPlayer)
+local replicatedStorage = cloneref(game:GetService("ReplicatedStorage"))
+
+local object = {
+    speed = guilib.blatant.CreateSection('Speed'),
+    esp = guilib.render.CreateSection('ESP')
+}
+
+local run = function(boolean) boolean() end
